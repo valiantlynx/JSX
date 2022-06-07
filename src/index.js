fd2bf443d8dd5013244a98c9bd9246a666d38786
valiantlynx <86688436+valiantlynx@users.noreply.github.com>
2022-06-07 02:50:31 +0200

    alt way for rendering pics

--- src/index.js
+++ src/index.js
@@ -5,29 +5,15 @@ const fName = "Valiant";
 const lName = "Lynx";
 const luckyNo = Math.round(Math.random() * 10);
 
+const img = "https://picsum.photos/200";
+
 ReactDOM.render(
   <div>
     <h1 className="heading" contentEditable="true" spellCheck="false">
       Hello {`${fName} ${lName}`}
     </h1>
     <p>Your lucky number is {luckyNo}</p>
-    <div>
-      <img
-        className="dreams-img"
-        src="https://images.fineartamerica.com/images/artworkimages/mediumlarge/2/astronomical-clock-bruce-rolff.jpg"
-        alt="time"
-      ></img>
-      <img
-        className="dreams-img"
-        src="https://imageio.forbes.com/specials-images/imageserve/62557e415e159fd6eab56ecb/Render-of-exterior-cabin-of-Spaceship-Neptune-by-Space-Perspective/960x0.jpg?format=jpg&width=960"
-        alt="space"
-      ></img>
-      <img
-        className="dreams-img"
-        src="https://www.globalfocusmagazine.com/wp-content/uploads/2020/02/Engaging_with_technology-scaled.jpg"
-        alt="technology"
-      ></img>
-    </div>
+    <img src={img + "?grayscale"} alt="sorry" />
   </div>,
   document.getElementById("root")
 );
