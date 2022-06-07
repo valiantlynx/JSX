db2deaaded36bc5d6789d1dc0ace7f2c45f2339d
valiantlynx <86688436+valiantlynx@users.noreply.github.com>
2022-06-07 16:28:39 +0200

    more styling option inline

--- src/index.js
+++ src/index.js
@@ -6,13 +6,19 @@ const lName = "Lynx";
 const luckyNo = Math.round(Math.random() * 10);
 
 const img = "https://picsum.photos/200";
+//the styling has to be done as js object and the key will no longer be kebab but camel case
+const customStyle = {
+  color: "red",
+  fontSize: "20px",
+  border: "1px solid black"
+};
 
 ReactDOM.render(
   <div>
     <h1 className="heading" contentEditable="true" spellCheck="false">
       Hello {`${fName} ${lName}`}
     </h1>
-    <p>Your lucky number is {luckyNo}</p>
+    <p style={customStyle}>Your lucky number is {luckyNo}</p>
     <img src={img + "?grayscale"} alt="sorry" />
   </div>,
   document.getElementById("root")
